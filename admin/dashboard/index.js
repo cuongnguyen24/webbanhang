afbf287b6e0b128ee41250a1d31299ec89bb4b78
Mons with Friend <daodinhbinh612004@gmail.com>
2024-06-21 14:41:10 +0700

    webbanhang

--- admin/dashboard/index.js
+++ admin/dashboard/index.js
@@ -2,7 +2,7 @@ const sideMenu = document.querySelector("aside");
 const menuBtn = document.querySelector("#menu_btn");
 const closeBtn = document.querySelector("#close-btn");
 const themeToggler = document.querySelector(".theme-toggler");
-const sideBarActive = document.querySelectorAll(".x");
+
 menuBtn.addEventListener('click', () => {
     sideMenu.style.display = 'block';
 })
@@ -13,12 +13,22 @@ closeBtn.addEventListener('click', () => {
 })
 
 //change
-
+var test = themeToggler.querySelector('i:nth-child(1)').className.split(' ');
+var nameTheme = "";
 themeToggler.addEventListener('click', () => {
     document.body.classList.toggle('dark-them-variables');
 
     themeToggler.querySelector('i:nth-child(1)').classList.toggle('active');
     themeToggler.querySelector('i:nth-child(2)').classList.toggle('active');
-})
 
-console.log(sideBarActive.length);
+    if (test[2] == "active") {
+        nameTheme = "light";
+    } else {
+        nameTheme = 'dark';
+    }
+
+})
+var nameTheme = "theme = " + nameTheme;
+document.cookie = nameTheme
+var theme = document.cookie;
+console.log(nameTheme);
