10588fcc28b4ce943ffda9218b345f2dcb95df72
Mons with Friend <daodinhbinh612004@gmail.com>
2024-06-23 23:06:42 +0700

    update1

--- collections/includes/connect.php
+++ collections/includes/connect.php
@@ -1,6 +1,6 @@
 <?php
 
-    $conn = mysqli_connect('localhost', 'root', '', 'websitebanhang');
+    $conn = mysqli_connect('localhost', 'root', '', 'webbanhang');
     if (!$conn) {
         die("Connection failed: " . mysqli_connect_error());
     }
