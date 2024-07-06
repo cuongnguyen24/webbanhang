83b11807f20e9ae67cb70adcd2f9938b4310f358
cuongnguyen24 <cuongngba7@gmail.com>
2024-07-06 18:36:58 +0700

    update search

--- user/search.php
+++ user/search.php
@@ -1,61 +1,147 @@
 <?php
-require_once '../admin/connect.php';
+session_start();
+// $username = $_SESSION["username"]; // Lấy tên tài khoản từ session
+require_once($_SERVER['DOCUMENT_ROOT'] . '/webbanhang/admin/connect.php');
+
+
+// lấy mã danh mục theo link 
+
+
+
+// Lấy dữ liệu từ CSDL
+// lấy tên danh mục dựa trên url 
+
+
+$URI = $_SERVER['REQUEST_URI'];
+
+$query1 = "  SELECT * 
+            FROM danhmuc
+            WHERE url = '$URI' ";
+$result1 = mysqli_query($conn, $query1);
+
+$madanhmuc;
+$tendanhmuc;
+if (mysqli_num_rows($result1) > 0) {
+    while ($row = mysqli_fetch_assoc($result1)) {
+        $madanhmuc = $row['maDanhMuc'];
+        $tendanhmuc = $row['tenDanhMuc'];
+    }
+}
+
+$searchTerm = $_GET['q'];
+$sql = "SELECT * FROM sanpham WHERE tenSanPham LIKE '%$searchTerm%'
+";
+$result = mysqli_query($conn, $sql);
+$sanphams = [];
+if (mysqli_num_rows($result) > 0) {
+    while ($row = mysqli_fetch_assoc($result)) {
+        $sanphams[] = $row;
+    }
+}
+
+
+
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Document</title>
-    <link rel="stylesheet" href="../assets/style.css" />
-    <link rel="stylesheet" href="../assets/reset.css" />
-    <link rel="stylesheet" href="../assets/Cuongstyle.css" />
+    <title>Khách hàng</title>
+    <link rel="stylesheet" href="/webbanhang/assets/style.css" />
+    <link rel="stylesheet" href="/webbanhang/assets/reset.css" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 </head>
 
 <body>
-    <?php include '../layout/header.php'; ?>
+    <?php
 
-    <div class="home_product_slider_wrap_body">
-        <div class="tab_content">
-            <?php
-            // Đoạn mã PHP xử lý tìm kiếm
-            if (isset($_GET['q'])) {
-                $searchTerm = $_GET['q'];
+    include($_SERVER['DOCUMENT_ROOT'] . '/webbanhang/collections/includes/login.php');
+    include($_SERVER['DOCUMENT_ROOT'] . '/webbanhang/layout/header.php');
+
+    ?>
+    <div class="main-layout">
+        <div class="collection template-collection">
+            <div class="breadcrum-shop ">
+                <div class="breadcrum-wrap container container-xl">
+                    <ol class="breadcrum">
+                        <li class="breadcrum-item">
+                            <a href="">Tìm kiếm</a>
+                        </li>
 
 
-                $query = "SELECT * FROM sanpham WHERE tenSanPham LIKE '%$searchTerm%'";
-                $result = mysqli_query($conn, $query);
+                    </ol>
+                </div>
+            </div>
 
-                if (mysqli_num_rows($result) > 0) {
-                    // Hiển thị kết quả tìm kiếm
-                    echo '<div class="wrapper">';
-                    echo '<ul class="container">';
-                    while ($row = mysqli_fetch_assoc($result)) {
-                        echo '<li class="pro">';
-                        echo '<div class="pro_container">';
-                        echo '<a href="#">';
-                        echo '<img class="img" src="' . $row["duongDanAnhChung"] . '" alt="">';
-                        echo '</a>';
-                        echo '<h3>';
-                        echo '<a href="#">' . $row["tenSanPham"] . '</a>';
-                        echo '</h3>';
-                        echo '<span>' . number_format($row["giaBan"], 0, ',', '.') . '₫</span>';
-                        echo '</div>';
-                        echo '</li>';
-                    }
-                    echo '</ul>';
-                    echo '</div>';
-                } else {
-                    echo '<p>Không tìm thấy sản phẩm nào.</p>';
-                }
-            }
+            <div class="container">
+                <div class="collection-wrap collection-wrap-product">
+                    <!-- ------------------PRODUCT LIST------------------------------- -->
+                    <div class="collection-wrap-product-list">
+                        <?php if (!empty($sanphams)) : ?>
+                            <?php foreach ($sanphams as $sanpham) : ?>
+                                <!-- PRODUCT -->
+                                <div class="pro-loop">
+                                    <div class="pro-loop-wrap">
+                                        <div class="pro-loop-image">
+                                            <a href="<?php echo $sanpham['chitietsp']; ?>" class="pro-loop-image-item">
+                                                <?php
+                                                $q1 = "SELECT duongDanAnh from anhsanpham where maSanPham ='" . $sanpham['maSanPham'] . "'";
+                                                $source = mysqli_query($conn, $q1);
+                                                $row1 = mysqli_fetch_assoc($source);
 
-            ?>
+                                                ?>
+                                                <picture>
+                                                    <source srcset="<?php echo '/webbanhang/admin/dashboard/products/' . $row1["duongDanAnh"]; ?>" data-srcset="<?php echo '/webbanhang/admin/dashboard/products/' . $row1["duongDanAnh"]; ?>" media="(max-width: 767px)" alt="img <?php echo $sanpham['tenSanPham']; ?>">
+                                                    <img class=" lazyloaded" src="<?php echo '/webbanhang/admin/dashboard/products/' . $row1["duongDanAnh"]; ?>" data-src="<?php echo '/webbanhang/admin/dashboard/products/' . $row1["duongDanAnh"]; ?>" alt="<?php echo $sanpham['tenSanPham']; ?>" style="max-width: 237.5px;">
+                                                </picture>
+                                                <picture>
+                                                    <source srcset="<?php echo '/webbanhang/admin/dashboard/products/' . $row1["duongDanAnh"]; ?>" data-srcset="<?php echo '/webbanhang/admin/dashboard/products/' . $row1["duongDanAnh"]; ?>" media="(max-width: 767px)" alt="img <?php echo $sanpham['tenSanPham']; ?>">
+                                                    <img class=" lazyloaded" src="<?php echo '/webbanhang/admin/dashboard/products/' . $row1["duongDanAnh"]; ?>" data-src="<?php echo '/webbanhang/admin/dashboard/products/' . $row1["duongDanAnh"]; ?>" alt="<?php echo $sanpham['tenSanPham']; ?>" style="max-width: 237.5px;">
+                                                </picture>
+                                            </a>
+
+                                            <div class="pro-loop-cart-icon">
+                                                <button type="button" class="setQuickview" data-handle="bo-lien-coc-mau-xanh-da-troi">
+                                                    <img src="https://file.hstatic.net/200000692427/file/asset_2_901a91642639466aa75b2019a34ccebd.svg" alt="add to cart">
+                                                    <span>Thêm vào giỏ hàng</span>
+                                                </button>
+                                            </div>
+                                        </div>
+                                        <h3 class="pro-loop-name">
+                                            <a href="/products/bo-lien-coc-mau-xanh-da-troi" title="<?php echo $sanpham['tenSanPham']; ?>"><?php echo $sanpham['tenSanPham']; ?></a>
+                                        </h3>
+                                        <div class="pro-loop-price">
+                                            <span><?php echo number_format($sanpham['giaBan'], 0, ',', '.'); ?>đ</span>
+                                        </div>
+
+
+                                    </div>
+                                </div>
+                            <?php endforeach; ?>
+                        <?php else : ?>
+                            <p>Không có sản phẩm nào.</p>
+                        <?php endif; ?>
+
+                    </div>
+                    <!-------------------------------------------------- END PRODUCT ----------------------------------->
+
+
+                </div>
+                <!-- -----------------------------------END PRODUCT LIST -------------------------------------------------------- -->
+            </div>
         </div>
     </div>
-    <?php include '../layout/footer.php'; ?>
+    </div>
+    </div>
+
+    <?php
+    include($_SERVER['DOCUMENT_ROOT'] . '/webbanhang/layout/footer.php');
+    ?>
+
+
 </body>
 
 </html>
