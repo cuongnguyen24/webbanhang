614db623fa4f844033dcbdaeb1aae821c58d412c
cuongnguyen24 <cuongngba7@gmail.com>
2025-04-23 15:44:56 +0700

    stop tracking connect.php

--- collections/includes/connect.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-    $conn = mysqli_connect('192.168.1.208', 'myuser', '12ssss21', 'myproject_db');
-    if (!$conn) {
-        die("Connection failed: " . mysqli_connect_error());
-    }
-
-?>
