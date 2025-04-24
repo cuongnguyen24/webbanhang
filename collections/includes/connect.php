0de494378b184531ab3fbcc1a09a754f9ec24392
Coangkuxm <hquang23102004@gmail.com>
2025-04-24 00:39:23 +0000

    update quang3

--- collections/includes/connect.php
+++ collections/includes/connect.php
@@ -1,6 +1,6 @@
 <?php
 
-    $conn = mysqli_connect('192.168.1.208', 'myuser', '12ssss21', 'myproject_db');
+    $conn = mysqli_connect('localhost', 'myuser2', 'Quang2004@', 'webbanhang2');
     if (!$conn) {
         die("Connection failed: " . mysqli_connect_error());
     }
